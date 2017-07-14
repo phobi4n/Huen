7ca565a5d626ec019c61deede22d0c9ef4e68bed
Mark <magpie240@gmx.com>
2017-07-15 05:30:49 +1000

    Add an uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+    echo "You need to run me as sudo"
+    exit 1
+fi
+
+if [ -f Makefile ]; then
+    make distclean
+fi
+
+if [ -d /usr/share/huen ]; then
+	rm -rv /usr/share/huen
+fi
+
+if [ -x /usr/bin/Huen ]; then
+	rm -v /usr/bin/Huen
+fi
+
+if [ -f /usr/share/applications/Huen.desktop ]; then
+	rm -v /usr/share/applications/Huen.desktop
+fi
