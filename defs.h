f591f73fee63a4a0d734557132a5cf707dd9cf72
Mark <smithmark347@gmail.com>
2017-11-16 07:58:27 +1100

    defs

--- defs.h
+++ defs.h
@@ -2,17 +2,17 @@
 #define DEFS_H
 
 // Saturation adjustment
-#define SATURATION_MULTIPLIER 1.0
+#define SATURATION_MULTIPLIER 1.7
 
 // Level at which to change dark text on light background [0 - 255]
 #define DARK_THRESHOLD 162
 
 // Text colours (quoted triplets)
 #define NORMAL_TEXT "255,255,255"
-#define DARK_TEXT "34,34,34"
+#define DARK_TEXT "16,16,16"
 
 // Backgrounds (quoted triplets)
-#define NORMAL_DIALOG "10,12,15"
-#define LIGHT_DIALOG "248,252,252"
+#define NORMAL_DIALOG "16,16,16"
+#define LIGHT_DIALOG "248,248,248"
 
 #endif // DEFS_H
